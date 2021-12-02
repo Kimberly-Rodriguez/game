fdea711b8feaaa9ebbda1968544561f10721fdd4
kimberly Rodriguez <kimberlyrodriguez@kimberlys-iMac.attlocal.net>
2021-12-02 15:45:00 -0800

    setting up the play again + call option for the function palyGame to be called

--- script.js
+++ script.js
@@ -1,88 +1,58 @@
-// set score chart 
+// set score chart
 let wins = 0;
 let losses = 0;
 let ties = 0;
 
 //Array showing the option available for the computer to select from
-let options = [ "R", "P", "S"]; 
+let options = ["R", "P", "S"];
 
-
-let playGame = function() {
+let playGame = function () {
   // ask user for choice
- let userOptions = window.prompt("Enter R, P, or S");
+  let userOptions = window.prompt("Enter R, P, or S");
 
- // if user selects Cancel -- end function
-  if (!userOptions){
-  return;
+  // if user selects Cancel -- end function
+  if (!userOptions) {
+    return;
   }
   // Convert to uppercase to make the comparison easier to do
   userOptions = userOptions.toUpperCase();
 
-
   // Get random index from array of options
   let index = Math.floor(Math.random() * options.length);
-  let computerChoice = option[index];
+  let computerChoice = options[index];
 
   window.alert("the computer selected " + computerChoice);
 
- // If there is a tie between user and computer
- if (userOptions === computerChoice) {
-   ties++
-   window.alert("it's a tie");
-  
- } else if ((userOptions === "R" && computerChoice === "S") ||
-           (userOptions === "P" && computerChoice === "R")){
+  // If there is a tie between user and computer
+  if (userOptions === computerChoice) {
+    ties++;
+    window.alert("it's a tie! ");
 
- }
-
-
-
-}
-
-
-
-let computer = function(){
-if("Rock" === true ) {
-  console.log ("R")
-} else if ("Paper" === true){
-  console("P")
-} else if ("Scissors" === true){
-  console.log("S")
-} else {
-  console.log("not match")
-}
-return computer
-}
-
-computer();
-
-let player = function(){
-  if("Rock" === true ) {
-    console.log ("R")
-  } else if ("Paper" === true){
-    console("P")
-  } else if ("Scissors" === true){
-    console.log("S")
+  // verifying every win condion possible for the player
+  } else if (
+    (userOptions === "R" && computerChoice === "S") ||
+    (userOptions === "P" && computerChoice === "R") ||
+    (userOptions === "S" && computerChoice === "P")
+  ) {
+    wins++;
+    window.alert("You win!");
+  //If above condition didn't render assume player lost points
   } else {
-    console.log("not match")
+    losses++;
+    window.alert("You lost!");
   }
-  return player
+  // print stats with line breaks
+   window.alert (
+     "Stats:\nWinds: " + wins + "\nLosses:" + losses + "\nTies:" + ties
+   );
+  // Ask user to play again
+  let playAgain = window.confirm("Do you want to play again?");
 
-}
-
-player();
-
-function tied(){
-  if ("Rock" === "Rock"){
-    console.log("tied")
-  } else if ("Paper" === "Paper"){
-    console.log("tied")
-  } else if ("Scissors" === "Scissors"){
-    console.log("tied")
-  } else {
-    console.log("not tied")
+  //if user pressed ok, start the function again
+  if (playAgain){
+    playGame();
   }
-  return
-}
+};
 
-tied();
+// start the game for the 1st time
+playGame();
